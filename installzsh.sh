c3a28fc4567eacc96edcd6f8dad3477a7844b756
Xie Zheng <xie.zheng@broadcom.com>
2024-10-30 13:51:16 +0800

    not set zsh as default shell

--- installzsh.sh
+++ installzsh.sh
@@ -88,6 +88,7 @@ git clone https://github.com/zsh-users/zsh-history-substring-search .zsh/zsh-his
 git clone https://github.com/jeffreytse/zsh-vi-mode.git .zsh/.zsh-vi-mode
 
 # shellcheck disable=SC2016
+echo 'export PATH=$PATH:/root/bin' >> ~/.zshrc
 echo 'export PATH=$PATH:/root/.local/bin' >> ~/.zshrc
 echo 'eval "$(oh-my-posh init zsh  --config ~/oh-my-posh/themes/catppuccin.omp.json)"' >> ~/.zshrc
 # shellcheck disable=SC1090
